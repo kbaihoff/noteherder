b84488bb485088f8febc3c1c520225572a22dd3b
Kai Hoffman <hoffm154@purdue.edu>
2017-06-14 15:16:11 -0400

    List notes using .map()

--- src/NoteList.js
+++ src/NoteList.js
@@ -2,47 +2,51 @@ import React from 'react'
 
 import './NoteList.css'
 
-const NoteList = () => {
+class NoteList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notes: [
+        {
+          noteTitle: "Citizens of distant epochs",
+          noteContent: "Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?",
+        },
+
+        {
+          noteTitle: "Preserve and cherish that pale blue dot",
+          noteContent: "network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!",
+        },
+
+        {
+          noteTitle: "Laws of physics",
+          noteContent: "Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas",
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="NoteList">
+        <h3>Notes</h3>
+        <ul id="notes">
+          {this.state.notes.map((note, i) => <ListNotes key={i} note={note}/>)}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function ListNotes(props) {
   return (
-    <div className="NoteList">
-      <h3>Notes</h3>
-      <ul id="notes">
-        <li>
-          <div className="note">
-            <div className="note-title">
-              Citizens of distant epochs
-            </div>
-            <div className="note-body">
-              <p>
-                Sea of Tranquility the ash of stellar alchemy vastness is bearable only through love bits of moving fluff are creatures of the cosmos, consciousness a still more glorious dawn awaits two ghostly white figures in coveralls and helmets are soflty dancing tingling of the spine, concept of the number one brain is the seed of intelligence are creatures of the cosmos?
-              </p>
-            </div>
-          </div>
-        </li>
-
-        <li>
-            <div className="note">
-              <div className="note-title">Preserve and cherish that pale blue dot </div>
-              <div className="note-body">
-                <p>
-                  network of wormholes a billion trillion the only home we've ever known light years dream of the mind's eye. Intelligent beings!
-                </p>
-              </div>
-            </div>
-          </li>
-
-          <li>
-            <div className="note">
-              <div className="note-title">Laws of physics</div>
-              <div className="note-body">
-                <p>
-                  Cambrian explosion radio telescope, circumnavigated citizens of distant epochs brain is the seed of intelligence two ghostly white figures in coveralls and helmets are soflty dancing galaxies inconspicuous motes of rock and gas
-                </p>
-              </div>
-            </div>
-          </li>
-      </ul>
-    </div>
+    <li>
+      <div className="note">
+        <div className="note-title">{props.note.noteTitle}</div>
+        <div className="note-body">
+          <p>{props.note.noteContent}</p>
+        </div>
+      </div>
+    </li>
   )
 }
 
