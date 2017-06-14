1d01265b7bd3c767af1d823f4b535ff8cc4775cf
Kai Hoffman <hoffm154@purdue.edu>
2017-06-14 14:02:36 -0400

    Make page look almost presentable

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,9 +1,21 @@
 import React from 'react'
+
 import './NoteForm.css'
 
 class NoteForm extends React.Component {
   render() {
-    return <p>FORM!</p>
+    return (
+      <div className="NoteForm">
+        <form>
+          <p>
+            <input type="text" name="title" placeholder="Title your note" value="" />
+          </p>
+          <p>
+            <textarea name="body" cols="30" rows="10" placeholder="Just start typing..."></textarea>
+          </p>
+        </form>
+      </div>
+    )
   }
 }
 
