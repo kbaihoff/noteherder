224dd1603e23242e299097861222381539d31cf8
Kai Hoffman <hoffm154@purdue.edu>
2017-06-20 10:17:23 -0400

    Update Firebase example

--- src/Base.example.js
+++ src/Base.example.js
@@ -1,6 +1,7 @@
 import Rebase from 're-base'
 import firebase from 'firebase/app'
 import database from 'firebase/database'
+import 'firebase/auth'
 
 const app = firebase.initializeApp({
   apiKey: "YOUR API KEY",
@@ -12,5 +13,9 @@ const app = firebase.initializeApp({
 })
 
 const db = database(app)
+
+export const auth = app.auth()
+export const githubProvider = new firebase.auth.GithubAuthProvider()
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
 
 export default Rebase.createClass(db)
