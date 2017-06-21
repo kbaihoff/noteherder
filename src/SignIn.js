4719959da0cc5d4657219382b23399b84ba0e748
Kai Hoffman <hoffm154@purdue.edu>
2017-06-21 14:26:39 -0400

    Style Github and Google login buttons

--- src/SignIn.js
+++ src/SignIn.js
@@ -11,8 +11,14 @@ const SignIn = () => {
 
   return (
     <div>
-      <button className="SignIn Github" onClick={() => authenticate(githubProvider)}>Sign In With GitHub</button>
-      <button className="SignIn Google" onClick={() => authenticate(googleProvider)}>Sign In With Google</button>
+      <button className="SignIn Github" onClick={() => authenticate(githubProvider)}>
+        <img src="https://www.coderbox.me/images/github.png" alt="github logo" className="logo"/>
+        Sign In With GitHub
+      </button>
+      <button className="SignIn Google" onClick={() => authenticate(googleProvider)}>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/600px-Google_%22G%22_Logo.svg.png" alt="google logo" className="logo" />
+        Sign In With Google
+      </button>
     </div>
   )
 }
