cff85ac04ae8f6d86ecd936a40112d674693cd95
Kai Hoffman <hoffm154@purdue.edu>
2017-06-21 11:04:05 -0400

    Delete form constructor and use props instead of state

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -3,56 +3,59 @@ import React from 'react'
 import './NoteForm.css'
 
 class NoteForm extends React.Component {
-  constructor(props) { // {notesAMLN, noteToOpenAMF, saveNoteFMA(), deleteNoteFMA()} from Main.js
-    super(props)
-    this.state = {
-      note: this.blankNote(),
-    }
-  }
+  // {notesAMLN, noteToOpenAMF, saveNoteFMA(), deleteNoteFMA()} from Main.js
+  // constructor(props) { 
+  //   super(props)
+  //   this.state = {
+  //     note: this.blankNote(),
+  //   }
+  // }
 
   componentWillReceiveProps = (newProps) => {
-    if (this.state.note.id !== newProps.noteToOpenAMF.id) { // this.props.currentNoteId === this.props.noteToOpenAMF.id
-      this.setState({ note: newProps.noteToOpenAMF })
-    }
+    // if (this.state.note.id !== newProps.noteToOpenAMF.id) { // this.props.currentNoteId === this.props.noteToOpenAMF.id
+    //   this.setState({ note: newProps.noteToOpenAMF })
+    // }
   }
 
-  blankNote = () => {
-    return {
-      id: null,
-      title: '',
-      body: '',
-    }
-  }
+  // blankNote = () => {
+  //   return {
+  //     id: null,
+  //     title: '',
+  //     body: '',
+  //   }
+  // }
 
   handleChanges = (ev) => {
-    const note = {...this.state.note}
+    const note = {...this.props.noteToOpenAMF}
     note[ev.target.name] = ev.target.value
-    this.setState(
-      { note: note },
-      () => this.props.saveNoteFMA(this.state.note)
-    )
+    // this.setState(
+    //   { note: note },
+    //   () => this.props.saveNoteFMA(this.state.note)
+    // )
+    this.props.saveNoteFMA(note)
     document.getElementById('save-button').style.backgroundColor = '#95E189'
   }
 
   handleDelete = (ev) => {
-    this.props.deleteNoteFMA(this.state.note)
-    this.setState({ note: this.blankNote() })
+    // this.props.deleteNoteFMA(this.state.note)
+    // this.setState({ note: this.blankNote() })
+    this.props.deleteNoteFMA(this.props.noteToOpenAMF)
   }
 
-  handleSave = (ev) => {
-    ev.preventDefault()
-    document.getElementById('save-button').style.backgroundColor = '#ADBDAB'
-  }
+  // handleSave = (ev) => {
+  //   ev.preventDefault()
+  //   document.getElementById('save-button').style.backgroundColor = '#ADBDAB'
+  // }
 
   render() {
     return (
       <div className="NoteForm">
         <form onSubmit={this.handleSave}>
           <p>
-            <input type="text" name="title" placeholder="Title your note" onChange={this.handleChanges} value={this.state.note.title} />
+            <input type="text" name="title" placeholder="Title your note" onChange={this.handleChanges} value={this.props.noteToOpenAMF.title} />
           </p>
           <p>
-            <textarea name="body" placeholder="Just start typing..." onChange={this.handleChanges} value={this.state.note.body}></textarea>
+            <textarea name="body" placeholder="Just start typing..." onChange={this.handleChanges} value={this.props.noteToOpenAMF.body}></textarea>
           </p>
           <button id="delete-button" onClick={this.handleDelete} >
             <i className="fa fa-trash-o" />
