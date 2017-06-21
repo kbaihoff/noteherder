96c41689171ffbabbda41132c3f1aced756d32d5
Kai Hoffman <hoffm154@purdue.edu>
2017-06-21 16:06:23 -0400

    Bold/unbold the whole form

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 import './NoteForm.css'
 
-class NoteForm extends React.Component {
-  // {notesAMLN, noteToOpenAMF, saveNoteFMA(), deleteNoteFMA()} from Main.js
+class NoteForm extends React.Component { // {notesAMLN, noteToOpenAMF, saveNoteFMA(), deleteNoteFMA()} from Main.js
+  state = {
+    selected: {},
+  }
+
   componentWillReceiveProps = (nextProps) => {
     if (nextProps.match.params.id) {
       const newId = nextProps.match.params.id
@@ -25,8 +28,21 @@ class NoteForm extends React.Component {
     this.props.saveNoteFMA(note)
   }
 
+  handleSelection = (ev) => {
+    console.log(ev.currentTarget.innerHTML)
+    ev.currentTarget.value = document.getSelection().toString().bold()
+    // document.getSelection().baseNode.toggleClass('bold')
+    // this.setState({ selected: document.getSelection() })
+    // document.getElementsByName("body")[0].textContent = document.getSelection().toString().bold()
+    // document.getElementsByName("body")[0].style.fontWeight = 'bold'
+    // console.log(this.state.selected, document.getSelection())
+    // console.log(document.querySelector('textarea').value)
+    // document.querySelector('textarea').value = document.getSelection().toString().bold()
+  }
+
   handleBold = (ev) => {
-    console.log('BOLD!')
+    ev.preventDefault()
+    document.querySelector('textarea').style.fontWeight = (document.querySelector('textarea').style.fontWeight === 'bold' ? 'normal' : 'bold')
   }
 
   handleDelete = (ev) => {
@@ -40,11 +56,11 @@ class NoteForm extends React.Component {
           <p>
             <input type="text" name="title" placeholder="Title your note" onChange={this.handleChanges} value={this.props.noteToOpenAMF.title} />
           </p>
-          <button id="bold-button" type="button" onClick={this.handleBold}>
+          <button id="bold-button" className="rich" type="button" onClick={this.handleBold}>
             <i className="fa fa-bold" />
           </button>
           <p>
-            <textarea name="body" placeholder="Just start typing..." onChange={this.handleChanges} value={this.props.noteToOpenAMF.body}></textarea>
+            <textarea name="body" placeholder="Just start typing..." onChange={this.handleChanges} value={this.props.noteToOpenAMF.body} />
           </p>
           <button id="delete-button" type="button" onClick={this.handleDelete} >
             <i className="fa fa-trash-o" />
